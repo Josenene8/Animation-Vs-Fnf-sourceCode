247e6ba2d5fe27047d939589f2793d9ec74c96dc
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-11 14:38:12 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -37,23 +37,44 @@ class OptionsState extends MusicBeatState
 	function openSelectedSubstate(label:String) {
 		switch(label) {
 			case 'Animation VS FNF Options':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.AVFSettingsSubState());
 			case 'Note Colors':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.GraphicsSettingsSubState());
+			case 'Controls':
+				#if android
+				removeVirtualPad();
+				#end
+				openSubState(new options.ControlsSubState());	
+				
 			case 'Visuals and UI':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
+				#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
-				case 'Android Controls':
-			  #if android
-			  MusicBeatState.switchState(new android.AndroidControlsMenu());
-			  #end
+				
 		}
 	}
 
@@ -95,8 +116,8 @@ class OptionsState extends MusicBeatState
 		ClientPrefs.saveSettings();
 
                 #if android
-		addVirtualPad(UP_DOWN, A_B);
-		#end
+		addVirtualPad(UP_DOWN, A_B_C);
+                #end
 			
 		super.create();
 	}
