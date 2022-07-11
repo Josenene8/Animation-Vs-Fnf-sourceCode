21e92cb7898e4e60ff1bbabdc87b7afe66177894
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-11 11:19:18 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,7 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
+import lime.system.System;
 
 class Main extends Sprite
 {
@@ -19,7 +20,9 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
-
+	
+        public static var path:String = System.applicationStorageDirectory;
+	
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
